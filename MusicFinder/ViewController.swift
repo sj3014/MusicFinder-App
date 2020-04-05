332c7a10c4de5fcd07adccd4c3c8e72a458abfcc
seongmoon06 <seongmoon06@gmail.com>
2020-04-06 02:19:23 +0900

    Initial Commit

---
+++ MusicFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicFinder
+//
+//  Created by Seong Moon Jo on 4/6/20.
+//  Copyright © 2020 Seong Moon Jo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
